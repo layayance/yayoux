569d44389dc9aac32dc629e97c4987c258b220d9
yaya.fane <yaya.fane@vinci-energies.net>
2023-06-01 17:28:58 +0200

    ajt color hover

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,12 +1,33 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Comment } from '../../../core/models/comment.model'
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+  styleUrls: ['./comments.component.scss'],
+  animations: [
+    trigger('listItem', [
+      state('default', style({
+        transform: 'scale(1)',
+        'background-color': 'white',
+        'z-index': 1
+      })),
+      state('active', style({
+        transform: 'scale(1.05)',
+        'background-color': 'rgb(201, 157, 242)',
+        'z-index': 2
+      })),
+      transition('default => active', [
+        animate('100ms ease-in-out')
+      ]),
+      transition('active => default', [
+        animate('500ms ease-in-out')
+      ]),
+    ])
+  ]
 })
 export class CommentsComponent implements OnInit {
 
@@ -14,21 +35,33 @@ export class CommentsComponent implements OnInit {
   @Output() newComment = new EventEmitter<string>();
 
   commentCtrl!: FormControl;
+  ListItemAnimationState: 'default' | 'active' = 'default';
 
-  constructor(private formBuilder: FormBuilder){}
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.commentCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(10)]);
   }
 
-  onLeaveComment(){
+  onLeaveComment() {
     if (this.commentCtrl.invalid) {
       return;
     }
-    console.log("dddd");
     this.newComment.emit(this.commentCtrl.value);
-    console.log("dddd2");
     this.commentCtrl.reset();
-    console.log("dddd3");
+
+    // console.log("dddd");
+    // this.newComment.emit(this.commentCtrl.value);
+    // console.log("dddd2");
+    // this.commentCtrl.reset();
+    // console.log("dddd3");
+  }
+
+  onListItemMouseEnter() {
+    this.ListItemAnimationState = 'active';
+  }
+
+  onListItemMouseLeave() {
+    this.ListItemAnimationState = 'default';
   }
 }
